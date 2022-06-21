8327d2da23803740dc1f63be58d1c4fa661287d4
dnwlaos23 <dnwlaos23@gmail.com>
2022-06-21 14:01:51 +0900

    폴더 구조

---
+++ migrations/20220621005650-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        type: Sequelize.DATE
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      privateKey: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
