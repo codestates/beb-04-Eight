5312f4dca09422ed9afa2b4caebc73fbed4f8b8a
dnwlaos23 <dnwlaos23@gmail.com>
2022-06-21 17:36:54 +0900

    Users 테이블 컬럼 추가

---
+++ migrations/20220621083404-alterUser.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     return Promise.all([
+      queryInterface.addColumn('Users', 'phone', { type:Sequelize.INTEGER(200) }),
+      queryInterface.addColumn('Users', 'userId', { type:Sequelize.STRING(200) })
+     ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
