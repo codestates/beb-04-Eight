1751d175c2aa15a3756b634f7044f9622c88db44
Ahreum0714 <yalin7145@gmail.com>
2022-06-22 18:25:37 +0900

    로그인 수정: 복호화 적용

--- controller/users/login.js
+++ controller/users/login.js
@@ -1,19 +1,31 @@
+const bcrypt = require("bcrypt");
 // 해당 모델의 인스턴스를 models/index.js에서 가져옵니다.
-const { Users } = require("../../models");
+const { User } = require("../../models");
 
 module.exports = {
   post: async (req, res) => {
-    const userInfo = await Users.findOne({
-      where: { userId: req.body.userId, password: req.body.password },
-    });
-    if (!userInfo) {
-      res.status(404).send({ message: "not authorized" });
-    } else {
-      req.session.save(function () {
-        req.session.userId = userInfo.userId;
-        res.json({ data: userInfo, message: "ok" });
+    try {
+      const { userId, password } = req.body;
+      // DB에서 사용자 찾기
+      const userInfo = await User.findOne({
+        where: { userId: userId },
       });
+      if (userInfo) {
+        // 비밀번호 체크 (입력한 비밀번호와 DB 비밀번호 비교)
+        const result = await bcrypt.compare(password, userInfo.password);
+        if (result) {
+          req.session.save(function () {
+            req.session.userId = userInfo.userId;
+            res.status(201).json({ message: "login success" });
+          });
+        } else {
+          res.status(401).json({ message: "비밀번호가 틀렸습니다" });
+        }
+      } else {
+        res.status(401).json({ message: "없는 아이디입니다" });
+      }
+    } catch (err) {
+      console.log(err);
     }
-    res.send("login router");
   },
 };
