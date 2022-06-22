7bd9a9a7db7e8cefc8039a9fd83906ea0d1e8d59
Ahreum0714 <yalin7145@gmail.com>
2022-06-22 17:34:33 +0900

    회원가입을 회원에게 지갑 생성 기능 구현

--- controller/users/join.js
+++ controller/users/join.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const lightwallet = require("eth-lightwallet");
 // 해당 모델의 인스턴스를 models/index.js에서 가져옵니다.
 const { User } = require("../../models");
 
@@ -22,15 +23,35 @@ module.exports = {
         // 아이디가 DB에 없으면
         // 비밀번호 해쉬화하고
         const hashedPassword = await bcrypt.hash(password, 12);
-        // DB에 저장
-        await User.create({
-          userId: userId,
-          password: hashedPassword,
-          userName: userName,
-          phone: phone,
-        });
-        // status 201 응답
-        res.status(201).send("create User!");
+
+        // 지갑을 생성하고
+        let mnemonic;
+        mnemonic = lightwallet.keystore.generateRandomSeed();
+        lightwallet.keystore.createVault(
+          {
+            password: password,
+            seedPhrase: mnemonic,
+            hdPathString: "m/0'/0'/0'",
+          },
+          function (err, ks) {
+            ks.keyFromPassword(password, function (err, pwDerivedKey) {
+              ks.generateNewAddress(pwDerivedKey, 1);
+
+              let address = ks.getAddresses().toString();
+              let privateKey = ks.exportPrivateKey(address, pwDerivedKey);
+              // DB에 지갑 주소와 privateKey 저장
+              User.create({
+                userId: userId,
+                password: hashedPassword,
+                userName: userName,
+                phone: phone,
+                address: address,
+                privateKey: privateKey,
+              });
+              res.status(201).send("create User!");
+            });
+          }
+        );
       }
     } catch (err) {
       console.log(err);
