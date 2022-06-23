f58724a778f0f945f904af2b193a657ceb7ade81
dnwlaos23 <dnwlaos23@gmail.com>
2022-06-23 16:31:26 +0900

    가나슈로부터 서버 계정으로 이더 전송 구현

--- server/controller/users/mypage.js
+++ server/controller/users/mypage.js
@@ -2,15 +2,10 @@ const { User } = require("../../models");
 
 module.exports = {
   get: async (req, res) => {
-    // TODO: 세션 객체에 담긴 값의 존재 여부에 따라 응답을 구현하세요.
-    // HINT: 세션 객체에 담긴 정보가 궁금하다면 req.session을 콘솔로 출력해보세요
 
     if (!req.session.userId) {
-      // your code here
       res.status(400).send({ message: "not authorized" });
     } else {
-      // your code here
-      // TODO: 데이터베이스에서 로그인한 사용자의 정보를 조회한 후 응답합니다.
       const result = await User.findOne({
         where: { userId: req.session.userId },
         attributes: { exclude: ["password", "privateKey"] },
--- server/controller/web3/ethFaucet.js
+++ server/controller/web3/ethFaucet.js
@@ -1,19 +1,53 @@
 const Web3 = require('web3');
+const { User } = require("../../models");
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
     post: async (req, res) => {
-      // TODO: 세션 아이디를 통해 고유한 세션 객체에 접근할 수 있습니다.
-      // 앞서 로그인시 세션 객체에 저장했던 값이 존재할 경우, 이미 로그인한 상태로 판단할 수 있습니다.
-      // 세션 객체에 담긴 값의 존재 여부에 따라 응답을 구현하세요.
-      //     서버의 세션 정보를 삭제해야 합니다.
-      // 클라이언트의 쿠키를 갱신해야 합니다.
-      // 서버가 클라이언트의 쿠키를 임의로 삭제할 수는 없습니다. 대신, set-cookie로 세션 아이디의 키값을 무효한 값으로 갱신해야 합니다.
-  
-      const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+      try {
+        const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545')); //가나슈로부터 web3 객체 생성
 
-      const accounts = await web3.eth.getAccounts();
+        const accounts = await web3.eth.getAccounts(); // 가나슈 계정 받아오기
 
-      console.log(accounts);
-      res.send(accounts);
+        const balance = await web3.eth.getBalance(accounts[0]); // 가나슈 계정 잔액 
+
+        const oneEth = 1000000000000000000;
+
+        const gasLimit = 2000000;
+
+        //userId가 server인 데이터의 address를 가져온다. 
+        const address = await User.findOne({
+          where: { userId: req.session.userId },
+          attributes:  ['address']
+        });
+
+        if(balance > (oneEth + gasLimit)){ // 가나슈 계정 잔액이 (1ETH + gas) 이상이라면
+          web3.eth.accounts.privateKeyToAccount(process.env.ETH_PRIVATEKEY); //계정 객체에 개인키를 등록한다. 
+
+          //트랜잭션에 서명
+          const signResult = await web3.eth.accounts.signTransaction({
+              to: address.dataValues.address.toString(),
+              value: oneEth.toString(),
+              gas: 2000000
+          }, process.env.ETH_PRIVATEKEY)
+
+          const sendResult = await web3.eth.sendSignedTransaction(signResult.rawTransaction) //트랜잭션 데이터가 매개변수로 들어감.
+          .on('receipt', console.log); //서명된 트랜잭션 전송
+
+          const serverBalance = await web3.eth.getBalance(address.dataValues.address.toString()); // 서버 계정의 잔액
+
+          res.status(200).json({message: "Faucet Successed",
+          data: {
+              address: address.dataValues.address.toString(),  // 사용자의 주소
+              balance: serverBalance,  // 사용자 이더 잔액
+              txHash: sendResult.transactionHash  // 이더 faucet 트랜잭션 해시
+          }});
+        } else {
+          res.status(502).send('Error: Faucet Transaction Failed')
+        }
+      } catch (error) {
+        console.log(err);
+      }
     },
   };
