b023fa826fd7cbc499a36f724f9cbce7612b281a
Ahreum0714 <yalin7145@gmail.com>
2022-06-24 03:18:24 +0900

    Refactoring - 가독성을 위해 else문 제거하고 if문으로만 구현

--- server/controller/users/join.js
+++ server/controller/users/join.js
@@ -18,41 +18,40 @@ module.exports = {
       // 아이디가 DB에 있으면
       if (checkID) {
         // status code 409
-        return res.status(409).send("이미 사용중인 아이디입니다.");
-      } else {
-        // 아이디가 DB에 없으면
-        // 비밀번호 해쉬화하고
-        const hashedPassword = await bcrypt.hash(password, 12);
-
-        // 지갑을 생성하고
-        let mnemonic;
-        mnemonic = lightwallet.keystore.generateRandomSeed();
-        lightwallet.keystore.createVault(
-          {
-            password: password,
-            seedPhrase: mnemonic,
-            hdPathString: "m/0'/0'/0'",
-          },
-          function (err, ks) {
-            ks.keyFromPassword(password, function (err, pwDerivedKey) {
-              ks.generateNewAddress(pwDerivedKey, 1);
-
-              let address = ks.getAddresses().toString();
-              let privateKey = ks.exportPrivateKey(address, pwDerivedKey);
-              // DB에 지갑 주소와 privateKey 저장
-              User.create({
-                userId: userId,
-                password: hashedPassword,
-                userName: userName,
-                phone: phone,
-                address: address,
-                privateKey: privateKey,
-              });
-              res.status(201).send("create User!");
-            });
-          }
-        );
+        return res.status(409).json({ message: "이미 사용중인 아이디입니다." });
       }
+      // 아이디가 DB에 없으면
+      // 비밀번호 해쉬화하고
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      // 지갑을 생성하고
+      let mnemonic;
+      mnemonic = lightwallet.keystore.generateRandomSeed();
+      lightwallet.keystore.createVault(
+        {
+          password: password,
+          seedPhrase: mnemonic,
+          hdPathString: "m/0'/0'/0'",
+        },
+        function (err, ks) {
+          ks.keyFromPassword(password, function (err, pwDerivedKey) {
+            ks.generateNewAddress(pwDerivedKey, 1);
+
+            let address = ks.getAddresses().toString();
+            let privateKey = ks.exportPrivateKey(address, pwDerivedKey);
+            // DB에 지갑 주소와 privateKey 저장
+            User.create({
+              userId: userId,
+              password: hashedPassword,
+              userName: userName,
+              phone: phone,
+              address: address,
+              privateKey: privateKey,
+            });
+            res.status(201).json({ message: "create User!" });
+          });
+        }
+      );
     } catch (err) {
       console.log(err);
       next(err);
