93e8fd23faec1b87796b4eb76e30ecde4a5f642f
Ahreum0714 <yalin7145@gmail.com>
2022-06-27 15:09:38 +0900

    게시글 전체 목록 res 내용 수정

--- server/controller/board/boardList.js
+++ server/controller/board/boardList.js
@@ -1,9 +1,11 @@
-const { Board } = require("../../models");
+const { Board, User } = require("../../models");
 
 module.exports = {
   get: async (req, res) => {
     try {
-      const { count, rows } = await Board.findAndCountAll();
+      const { count, rows } = await Board.findAndCountAll({
+        include: { model: User, attributes: ["userId"] },
+      });
 
       res.status(200).json({
         message: "get board LIST success",
