7f59c74cca202738ae553d052e344cd8997369bb
Ahreum0714 <yalin7145@gmail.com>
2022-06-27 14:51:59 +0900

    게시글 수정 구현현 완료

--- server/controller/board/boardDetail.js
+++ server/controller/board/boardDetail.js
@@ -28,14 +28,51 @@ module.exports = {
     }
   },
   put: async (req, res) => {
-    console.log("Update board!");
-    res.status(200).json({
-      message: "get board DETAIL success",
-      data: data,
-    });
+    if (!req.session.userId) {
+      res.status(401).json({ message: "Not authorized" });
+    }
+
+    try {
+      const { id } = req.params;
+      const { title, content } = req.body;
+
+      const boardInfo = await Board.findOne({
+        where: { id: id },
+        attributes: ["user_id"],
+        include: {
+          model: User,
+          attributes: ["userId"],
+        },
+      });
+
+      if (req.session.userId !== boardInfo.User.userId) {
+        res.status(403).json({ message: "작성자만 글을 수정할 수 있습니다" });
+      }
+
+      const updatedRows = await Board.update(
+        {
+          title: title,
+          content: content,
+        },
+        {
+          where: { id: id },
+        }
+      );
+
+      res.status(200).json({
+        message: "Update board!",
+        data: {
+          id: id,
+          title: title,
+          content: content,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
   },
   delete: async (req, res) => {
-    console.log("board detail delete");
+    console.log("Delete board!");
     res.status(204);
   },
 };
