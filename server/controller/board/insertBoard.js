3d51d6c12a13a2225f947a4e1b5db49c23242570
Ahreum0714 <yalin7145@gmail.com>
2022-06-27 12:40:36 +0900

    게시글 작성 구현 완료

---
+++ server/controller/board/insertBoard.js
@@ -0,0 +1,38 @@
+const { Board } = require("../../models");
+const { User } = require("../../models");
+
+module.exports = {
+  post: async (req, res) => {
+    // ERROR: 미로그인 상태
+    if (!req.session.userId) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    try {
+      const { title, content } = req.body;
+      const userInfo = await User.findOne({
+        where: { userId: req.session.userId },
+        attributes: ["id", "userId"],
+      });
+
+      const result = await Board.create({
+        title: title,
+        user_id: userInfo.id,
+        content: content,
+      });
+
+      res.status(201).json({
+        message: "Create board!",
+        data: {
+          userId: userInfo.userId,
+          title: result.title,
+          content: result.content,
+          createdAt: result.createdAt + "+09:00",
+          updatedAt: result.updatedAt + "+09:00",
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
