98a68deefc014fab457a5b65bd43244a6ff20e56
arh714 <99606073+Ahreum0714@users.noreply.github.com>
2022-06-28 16:31:43 +0900

    Back - 더미 데이터 seed 추가 및 DB 컬럼 타입 수정 (#15)
    * 게시글 전체 목록 res 내용 수정
    
    * Boards.content 컬럼 타입 STRING -> TEXT로 변경 & 더미데이터 seed 추가

---
+++ server/seeders/20220628070216-test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    let datas = [];
+    for (let i = 1; i <= 5; i++) {
+      let obj = {
+        title: `dummy data title ${i}`,
+        user_id: i,
+        content: `dummy data post ${i}
+        What Is a Blockchain?
+A blockchain is a distributed database or ledger that is shared among the nodes of a computer network. As a database, a blockchain stores information electronically in digital format. Blockchains are best known for their crucial role in cryptocurrency systems, such as Bitcoin, for maintaining a secure and decentralized record of transactions. The innovation with a blockchain is that it guarantees the fidelity and security of a record of data and generates trust without the need for a trusted third party.`,
+      };
+      datas.push(obj);
+    }
+    return queryInterface.bulkInsert("Boards", datas, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
