2e53fbc48d50908c515afdf84f1ca45b6c81eb08
dnwlaos23 <dnwlaos23@gmail.com>
2022-06-28 13:46:18 +0900

    컨트랙트 배포 및 토큰 전송 수정, 토큰 교환 구현

--- server/controller/web3/serveToken.js
+++ server/controller/web3/serveToken.js
@@ -1,5 +1,6 @@
 const Web3 = require('web3');
 const abi = require('./abi');
+const { User } = require("../../models");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -12,58 +13,61 @@ module.exports ={
 
             const myContract = new web3.eth.Contract(abi, (process.env.CONTRACT_ADDRESS).toString());
 
-            // const address = await User.findOne({
-            //     where: { userId: req.session.userId },
-            //     attributes:  ['address']
-            //   });
+            const userData = await User.findOne({
+                where: { userId: req.session.userId },
+                attributes:  ['address']
+            });
+
+            const serverData = await User.findOne({
+                where: { userId: 'server' },
+                attributes:  ['address']
+            });
+
+            const serverAddress = serverData.dataValues.address.toString();
+
+            const userId = req.session.userId;
+            const address = userData.dataValues.address.toString();
+
+            const serveTokenAmount = 100000;
         
             //트랜잭션을 통해 보낼 ABI코드 인코딩
-            const data = myContract.methods.transfer('0xecf79a71ac1becf5f1a1002008931e5079cd00d5', 100000).encodeABI();
+            const data = myContract.methods.transfer(address, serveTokenAmount).encodeABI();
 
             async function getTOKENBalanceOf(address){ //해당 주소에 myContract로 생성된 토큰 잔액 보기 
                 return await myContract.methods.balanceOf(address).call();                        
             }   
 
-            const rawTransaction = {"to": (process.env.CONTRACT_ADDRESS).toString(), "gas": 100000, "data": data }; 
+            const serverBalance = await getTOKENBalanceOf(serverAddress); // 서버 계정의 토큰 잔액
 
-            console.log("test");
-            // web3.eth.accounts.signTransaction(rawTransaction, process.env.ETH_PRIVATEKEY) // 비밀키로 서명 후 트랜잭션 전송 
-            //     .then(signedTx => web3.eth.sendSignedTransaction(signedTx.rawTransaction))
-            //     //.then(function(receipt){ console.log("Transaction receipt: ", receipt); getETHBalanceOf();  })
-            //     .then(req => { 
-            //             /* The trx was done. Write your acctions here. For example getBalance */
-            //             getTOKENBalanceOf('0xecf79a71ac1becf5f1a1002008931e5079cd00d5').then ( balance => { console.log('0xecf79a71ac1becf5f1a1002008931e5079cd00d5' + " Token Balance: " + balance); });
-            //             return true;  
-            //     }) 
-            const blockNumber = await web3.eth.getBlockNumber();
+            const tokenSupply = 100000; // 유저에게 전송할 토큰의 양
 
+            if(serverBalance > tokenSupply){
+                const rawTransaction = {"to": (process.env.CONTRACT_ADDRESS).toString(), "gas": tokenSupply, "data": data }; 
 
-            const blockData = await web3.eth.getBlock(blockNumber)
-            console.log(blockData)
-            const transactionData = await web3.eth.getTransaction(blockData.transactions)
-            console.log(transactionData);
-
-            // Transaction.create({
-            //     blockNumber: ,
-            //     TransactionHash: ,
-            //     from: ,
-            //     to: ,
-            //     contractAddress: ,
-            //   });
-            res.status(200).json({
-                message: "Serving Successed",
-                // data: {
-                //     username: 'kimcoding',  // 사용자 이름
-                //     address: '0x32482...',  // 받는 계정의 주소
-                //     // txHash: '0x234C1...',  // 트랜잭션 해시
-                //     tokenBalance: balance, // 유저의 토큰 잔액
-                // }
-            })
-        } catch (error) {
+            web3.eth.accounts.signTransaction(rawTransaction, process.env.ETH_PRIVATEKEY) // 비밀키로 서명 후 트랜잭션 전송 
+                .then(signedTx => web3.eth.sendSignedTransaction(signedTx.rawTransaction))
+                //.then(function(receipt){ console.log("Transaction receipt: ", receipt); getETHBalanceOf();  })
+                .then(req => { 
+                        /* The trx was done. Write your acctions here. For example getBalance */
+                        getTOKENBalanceOf(address)
+                        .then ( balance => {
+                             const userBalance = balance;
+                             res.status(200).json({
+                                message: "Serving Successed",
+                                data: {
+                                    userId: userId,  // 사용자 아이디
+                                    address: address,  // 받는 계정의 주소
+                                    tokenBalance: userBalance, // 유저의 토큰 잔액
+                                }
+                            })
+                        });
+                }) 
+            } else {
+                res.status(502).send('Error: serveToken Transaction Faield')
+            }
             
+        } catch (error) {
+            console.log(error);
         }
-
-
-        
     }
 }
